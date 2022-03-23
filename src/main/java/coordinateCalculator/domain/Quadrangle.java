41d534b7479a84f3fdba98d4a3179ace68d8f85d
tyakamyz <tyakamyz@naver.com>
2022-03-23 14:53:58 +0900

    feat: 삼각형 main 메소드 추가

--- src/main/java/coordinateCalculator/domain/Quadrangle.java
+++ src/main/java/coordinateCalculator/domain/Quadrangle.java
@@ -37,19 +37,11 @@ public class Quadrangle extends CoordinateCalculator {
         lines.deduplicationLinesLength();
     }
 
-    public Line createLine(Point pointA, Point pointB){
-        Line line = new Line();
-        line.addPoint(pointA);
-        line.addPoint(pointB);
-
-        return line;
-    }
-
     public void createLines(){
-        lines.addLine(createLine(points.get(0), points.get(1)));
-        lines.addLine(createLine(points.get(1), points.get(2)));
-        lines.addLine(createLine(points.get(2), points.get(3)));
-        lines.addLine(createLine(points.get(3), points.get(0)));
+        lines.addLine(new Line(points.get(0), points.get(1)));
+        lines.addLine(new Line(points.get(1), points.get(2)));
+        lines.addLine(new Line(points.get(2), points.get(3)));
+        lines.addLine(new Line(points.get(3), points.get(0)));
     }
 
     public static void main(String[] args) throws IOException {
